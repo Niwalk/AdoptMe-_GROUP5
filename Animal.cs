2920687ca8c70388863e80a067932b30f1a47b4f
zerxt <190685240+zerxt@users.noreply.github.com>
2025-04-21 05:40:30 +0700

    Initializing Database

---
+++ Animal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptMe___GROUP5
+{
+    internal class Animal
+    {
+        String id; {  get; set; }
+        String name { get; set; }
+        String color { get; set; }
+        String species { get; set; }
+
+        String 
+    }
+}
