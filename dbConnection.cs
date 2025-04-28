2e0f07d3a1d06dd5232430f92b579de2ae93a749
zerxt <zerxt@no-replay.git>
2025-04-28 03:32:25 -0700

    added db Connection

---
+++ dbConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoptMe___GROUP5
+{
+    internal class dbConnection
+    {
+        private readonly string cs = "Data Source=;" +
+                     "Initial Catalog=students;" +
+                     "Integrated Security=True;" +
+                     "TrustServerCertificate=True;";
+
+    }
+}
